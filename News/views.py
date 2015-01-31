5d274ba97b68f259a1fa8880ab7596643b64f744
Rishab Hegde <reparadocs@pcp046908pcs.wireless.calpoly.edu>
2015-01-31 12:26:05 -0800

    gbag

--- News/views.py
+++ News/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from open_facebook import OpenFacebook
+from open_facebook import OpenFacebook, FacebookConnection
 from django_facebook.api import get_facebook_graph
 from django.http import HttpResponse
 import json
@@ -8,6 +8,7 @@ def news(request):
    if request.method == 'GET':
       graph = request.user.get_offline_graph()
       dicti = graph.get('/me/likes')
-      return HttpResponse(json.dumps(dicti))
+      data = FacebookConnection.request('/me/likes')
+      return HttpResponse(json.dumps(data))
 
       
