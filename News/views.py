b42dbf4a31fea9df78f070ab1019c0801d201cd0
Rishab Hegde <reparadocs@pcp046908pcs.wireless.calpoly.edu>
2015-01-31 12:40:53 -0800

    gbag

--- News/views.py
+++ News/views.py
@@ -7,7 +7,6 @@ import json
 def news(request):
    if request.method == 'GET':
 
-      graph = request.user.get_offline_graph()
-      return HttpResponse(graph.access_token())
+      return HttpResponse(request.user.get_likes())
 
       
