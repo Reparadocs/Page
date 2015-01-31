5f0a7bace753109563d5c86734f04e73e722e49a
Rishab Hegde <reparadocs@pcp046908pcs.wireless.calpoly.edu>
2015-01-31 11:09:20 -0800

    stufefnafl

--- News/views.py
+++ News/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from open_facebook import OpenFacebook
 from django_facebook.api import get_facebook_graph
+from django.http import HttpResponse
 
 def news(request):
    if request.method == 'GET':
